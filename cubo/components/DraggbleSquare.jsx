19c1f719cf031bfebb4d120cd296981c290d8d26
JADSON DANILO MACENA DA SILVA <a94864926@rn.senac.local>
2025-07-30 20:57:54 -0300

    cubo Atualizado

--- cubo/components/DraggbleSquare.jsx
+++ cubo/components/DraggbleSquare.jsx
@@ -1,1 +1,7 @@
-// Boa noite
+import { gesture, gestureDetector } from 'react-native-gesture-handler';
+import Animated, { 
+    useSharedValue,
+    useAnimatedStyle,
+    withSpring,
+    interpolate
+} from 'react-native-reanimated';
